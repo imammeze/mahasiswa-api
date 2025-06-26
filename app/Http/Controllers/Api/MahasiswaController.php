9928cc9ff234ca11358c7919db722e2c5b325c68
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2025-06-26 20:53:32 +0700

    feat (controller) : Implement index method in MahasiswaController to paginate and return Mahasiswa data

--- app/Http/Controllers/Api/MahasiswaController.php
+++ app/Http/Controllers/Api/MahasiswaController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\MahasiswaResource;
 
 class MahasiswaController extends Controller
 {
-    //
+    public function index()
+    {
+        $mahasiswas = Mahasiswa::latest()->paginate(5);
+
+        return new MahasiswaResource(true, 'List Data Mahasiswa', $mahasiswas);
+    }
 }
