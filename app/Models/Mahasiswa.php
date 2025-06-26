cb874d86afa8119c71f1802c33c61464de822d2f
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2025-06-26 19:54:14 +0700

    feat (model) : Add image accessor to Mahasiswa model

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Mahasiswa extends Model
@@ -10,4 +11,11 @@ class Mahasiswa extends Model
     use HasFactory;
 
     protected $fillable = ['nama', 'nim', 'prodi', 'image'];
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/mahasiswas/' . $image),
+        );
+    }
 }
