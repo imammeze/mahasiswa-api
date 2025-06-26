bffa26fe89f31abe08e3aa40cc1ca25e2de78211
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2025-06-26 21:36:57 +0700

    feat (controller) : Enhance MahasiswaController with store, show, update, and destroy methods for managing Mahasiswa data

--- app/Http/Controllers/Api/MahasiswaController.php
+++ app/Http/Controllers/Api/MahasiswaController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Models\Mahasiswa;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Resources\MahasiswaResource;
+use Illuminate\Support\Facades\Validator;
 
 class MahasiswaController extends Controller
 {
@@ -14,5 +16,87 @@ class MahasiswaController extends Controller
         $mahasiswas = Mahasiswa::latest()->paginate(5);
 
         return new MahasiswaResource(true, 'List Data Mahasiswa', $mahasiswas);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama'      => 'required',
+            'nim'       => 'required',
+            'prodi'     => 'required',
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('public/mahasiswas', $image->hashName());
+        
+        $mahasiswa = Mahasiswa::create([
+            'nama'      => $request->nama,
+            'nim'       => $request->nim,
+            'prodi'     => $request->prodi,
+            'image'     => $image->hashName(),
+        ]);
+
+        return new MahasiswaResource(true, 'Data Mahasiswa Berhasil Ditambahkan!', $mahasiswa);
+    }
+
+    public function show($id)
+    {
+        $mahasiswa = Mahasiswa::find($id);
+
+        return new MahasiswaResource(true, 'Detail Data Mahasiswa !', $mahasiswa);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'nama'      => 'required',
+            'nim'       => 'required',
+            'prodi'     => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+   
+        $mahasiswa = Mahasiswa::find($id);
+
+        if ($request->hasFile('image')) {
+
+            $image = $request->file('image');
+            $image->storeAs('public/mahasiswas', $image->hashName());
+
+            Storage::delete('public/mahasiswas/' . basename($mahasiswa->image));
+
+            $mahasiswa->update([
+                'nama'      => $request->nama,
+                'nim'       => $request->nim,
+                'prodi'     => $request->prodi,
+                'image'     => $image->hashName(),
+            ]);
+        } else {
+            $mahasiswa->update([
+                'nama'      => $request->nama,
+                'nim'       => $request->nim,
+                'prodi'     => $request->prodi,
+            ]);
+        }
+
+        return new MahasiswaResource(true, 'Data Mahasiswa Berhasil Diubah!', $mahasiswa);
+    }
+
+    public function destroy($id)
+    {
+        $mahasiswa = Mahasiswa::find($id);
+        
+        Storage::delete('public/mahasiswas/'.basename($mahasiswa->image));
+
+        $mahasiswa->delete();
+
+        return new MahasiswaResource(true, 'Data Mahasiswa Berhasil Dihapus!', null);
     }
 }
